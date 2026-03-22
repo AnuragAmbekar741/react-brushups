6759c0951d3cec262883a468ff38dfe402cb5de6
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2026-03-22 02:33:32 -0700

    deep equals

--- preparing-for-ui-interview-v2/src/problems/09-deep-equals/deep-equals.vanila.ts
+++ preparing-for-ui-interview-v2/src/problems/09-deep-equals/deep-equals.vanila.ts
@@ -3,18 +3,43 @@
 import { detectType } from '@course/utils'
 
 export function deepEquals(a: any, b: any, cache = new Map()): boolean {
+  if (a === b || (cache.has(a) && cache.get(a) === b)) {
+    return true
+  }
 
+  const [typeA, typeB] = [detectType(a), detectType(b)]
+
+  if (typeA !== typeB) {
+    return false
+  }
+
+  if (typeof a !== 'object') {
+    return a === b
+  }
+
+  const [keyA, keyB] = [new Set(Object.keys(a)), new Set(Object.keys(b))]
+
+  if (keyA.symmetricDifference(keyB).size > 0) {
+    return false
+  }
+  cache.set(a, b)
+  for (const key of keyA) {
+    if (!deepEquals(a[key], b[key], cache)) {
+      return false
+    }
+  }
+  return true
 }
 
 // --- Examples ---
 // Uncomment to test your implementation:
 
-// console.log(deepEquals(1, 1))                          // Expected: true
-// console.log(deepEquals('hello', 'hello'))               // Expected: true
-// console.log(deepEquals(null, undefined))                // Expected: false
-// console.log(deepEquals([1, 2, 3], [1, 2, 3]))          // Expected: true
-// console.log(deepEquals({ a: 1, b: 2 }, { b: 2, a: 1 })) // Expected: true
-// console.log(deepEquals({ a: 1 }, { a: 2 }))            // Expected: false
+console.log(deepEquals(1, 1)) // Expected: true
+console.log(deepEquals('hello', 'hello')) // Expected: true
+console.log(deepEquals(null, undefined)) // Expected: false
+console.log(deepEquals([1, 2, 3], [1, 2, 3])) // Expected: true
+console.log(deepEquals({ a: 1, b: 2 }, { b: 2, a: 1 })) // Expected: true
+console.log(deepEquals({ a: 1 }, { a: 2 })) // Expected: false
 
 // const a: any = { value: 1 }; a.self = a
 // const b: any = { value: 1 }; b.self = b
